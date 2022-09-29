bdcdac03e44f7ca70a1f8c35e1cc02dc5e9b4d63
anavarro38 <91505302+anavarro38@users.noreply.github.com>
2022-09-29 11:23:01 -0700

    Add files via upload

---
+++ AnimeStore/src/main/java/com/gcu/controller/MainMenuController.java
@@ -0,0 +1,33 @@
+package com.gcu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gcu.model.HomeModel;
+
+@Controller
+@RequestMapping("/home")
+public class MainMenuController {
+	
+	@GetMapping("/test1")
+	public String display(Model model) 
+	{
+		model.addAttribute("title", "Home Page");
+//		model.addAttribute("home",new HomeModel());
+		model.addAttribute("message", new String("AHHHHHHHHHHHHH"));
+		return "home";
+	}
+	
+	@GetMapping("/test2")
+	public ModelAndView printHello1() 
+	{
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("message", new String("Welcome to GAS"));
+		mv.addObject("message2", new String("AKA just another Generic Anime Store"));
+		mv.setViewName("home");
+		return mv;
+	}
+}
---
+++ AnimeStore/src/main/java/com/gcu/model/HomeModel.java
@@ -0,0 +1,12 @@
+package com.gcu.model;
+
+public class HomeModel {
+	private String intro;
+	
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+}
